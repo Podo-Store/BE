c5660e3be69e9cc5e961f1a24fa15e4dab582ce1
pipi-shortstocking <jannie435@naver.com>
2024-02-26 18:36:35 +0900

    initialize

---
+++ src/main/java/PodoeMarket/podoemarket/PodoemarketApplication.java
@@ -0,0 +1,13 @@
+package PodoeMarket.podoemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodoemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodoemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PodoeMarket/podoemarket/PodoemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package PodoeMarket.podoemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodoemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
