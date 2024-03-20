a5223d7bf78e1382745d3de62dfc2fb2ac33c26c
pipi-shortstocking <jannie435@naver.com>
2024-03-21 00:09:49 +0900

    DB 수정 중

---
+++ src/main/java/PodoeMarket/podoemarket/entity/QuestionEntity.java
@@ -0,0 +1,20 @@
+package PodoeMarket.podoemarket.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "question")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class QuestionEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+
+
+}
--- src/main/java/PodoeMarket/podoemarket/entity/UserEntity.java
+++ src/main/java/PodoeMarket/podoemarket/entity/UserEntity.java
@@ -5,7 +5,6 @@ import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
