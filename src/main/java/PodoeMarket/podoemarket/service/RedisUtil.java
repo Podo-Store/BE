455bf2deec5b2c69d1cc11f12fddb5489abe5f79
pipi-shortstocking <jannie435@naver.com>
2024-07-17 03:10:44 +0900

    FIX: Redis AWS 이관 작업

--- src/main/java/PodoeMarket/podoemarket/service/RedisUtil.java
+++ src/main/java/PodoeMarket/podoemarket/service/RedisUtil.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -23,19 +22,22 @@ public class RedisUtil {
             return "redis get 문제 발생";
         }
     }
+
     public void setData(String key,String value){//지정된 키(key)에 값을 저장하는 메서드
         ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
         valueOperations.set(key,value);
     }
+
     public void setDataExpire(String key,String value,long duration){//지정된 키(key)에 값을 저장하고, 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
         try{
             ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
             Duration expireDuration=Duration.ofSeconds(duration);
             valueOperations.set(key,value,expireDuration);
         } catch(Exception e) {
-            log.warn("redis set 문제 발생");
+            log.warn("redis set 문제 발생:{}", e);
         }
     }
+
     public void deleteData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 삭제하는 메서드
         redisTemplate.delete(key);
     }
