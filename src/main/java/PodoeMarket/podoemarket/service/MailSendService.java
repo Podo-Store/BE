33a2f584df496333075e973cf2b4ae364576ee06
pipi-shortstocking <jannie435@naver.com>
2024-07-22 17:37:43 +0900

    fix: 메일 전송 시, 주소가 아닌 이름으로 보내기

--- src/main/java/PodoeMarket/podoemarket/service/MailSendService.java
+++ src/main/java/PodoeMarket/podoemarket/service/MailSendService.java
@@ -9,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @RequiredArgsConstructor
@@ -67,7 +68,7 @@ public class MailSendService {
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");//이메일 메시지와 관련된 설정을 수행합니다.
             // true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
-            helper.setFrom(setFrom);//이메일의 발신자 주소 설정
+            helper.setFrom(setFrom, "포도상점");//이메일의 발신자 주소 설정
             helper.setTo(toMail);//이메일의 수신자 주소 설정
             helper.setSubject(title);//이메일의 제목을 설정
             helper.setText(content,true);//이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
@@ -75,6 +76,8 @@ public class MailSendService {
         } catch (MessagingException e) {//이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
             // 이러한 경우 MessagingException이 발생
             e.printStackTrace();//e.printStackTrace()는 예외를 기본 오류 스트림에 출력하는 메서드
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
         redisUtil.setDataExpire(Integer.toString(authNumber),toMail,60*5L);
     }
