8e135f9b8062d6fc4f1ee95d5791f9fc1dc2168a
pipi-shortstocking <jannie435@naver.com>
2024-07-22 16:40:00 +0900

    Fix: 작품 업로드 검증 추가(#59)

--- src/main/java/PodoeMarket/podoemarket/service/RegisterService.java
+++ src/main/java/PodoeMarket/podoemarket/service/RegisterService.java
@@ -1,5 +1,6 @@
 package PodoeMarket.podoemarket.service;
 
+import PodoeMarket.podoemarket.dto.ResponseDTO;
 import PodoeMarket.podoemarket.entity.ProductEntity;
 import PodoeMarket.podoemarket.repository.ProductRepository;
 import com.amazonaws.services.s3.AmazonS3;
@@ -7,6 +8,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,18 +32,25 @@ public class RegisterService {
         fileRepo.save(scriptEntity);
     }
 
-    public String uploadScript(MultipartFile file) throws IOException {
-        if (!Objects.equals(file.getContentType(), "application/pdf")) {
+    public String uploadScript(MultipartFile[] files) throws IOException {
+        if(files[0].isEmpty()) {
+            throw new RuntimeException("선택된 파일이 없음");
+        }
+        else if(files.length > 1) {
+            throw new RuntimeException("파일 수가 1개를 초과함");
+        }
+
+        if (!Objects.equals(files[0].getContentType(), "application/pdf")) {
             throw new RuntimeException("contentType is not PDF");
         }
 
-        String filePath = bucketFolder + file.getOriginalFilename();
+        String filePath = bucketFolder + files[0].getOriginalFilename();
 
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(files[0].getSize());
+        metadata.setContentType(files[0].getContentType());
 
-        amazonS3.putObject(bucket, filePath, file.getInputStream(), metadata);
+        amazonS3.putObject(bucket, filePath, files[0].getInputStream(), metadata);
 
         return amazonS3.getUrl(bucket, filePath).toString();
     }
