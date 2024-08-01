5177be042a3e1f566fd250f348da04058f20192c
pipi-shortstocking <jannie435@naver.com>
2024-08-02 01:18:55 +0900

    feat: 대본권 중복 구매 막기(#83)

---
+++ src/main/java/PodoeMarket/podoemarket/repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package PodoeMarket.podoemarket.repository;
+
+import PodoeMarket.podoemarket.entity.OrderItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long> {
+    OrderItemEntity findByProductId(UUID id);
+}
