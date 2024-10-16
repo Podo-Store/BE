84f48e2d29cea9e9a8c4f64d3c2db91fbd244676
pipi-shortstocking <jannie435@naver.com>
2024-10-16 21:54:58 +0900

    fix: https 설정

--- src/main/java/PodoeMarket/podoemarket/config/WebConfig.java
+++ src/main/java/PodoeMarket/podoemarket/config/WebConfig.java
@@ -56,7 +56,7 @@ public class WebConfig implements WebMvcConfigurer {
         config.setAllowCredentials(true);
 //        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
 //        config.setAllowedOriginPatterns(Arrays.asList("http://3.36.91.199"));
-        config.setAllowedOriginPatterns(Arrays.asList("http://www.podo-store.com"));
+        config.setAllowedOriginPatterns(Arrays.asList("https://www.podo-store.com"));
         config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
 
