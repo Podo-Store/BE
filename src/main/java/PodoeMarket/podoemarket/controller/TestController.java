765f5933fae49e5bd777609144d704163b29c8f1
pipi-shortstocking <jannie435@naver.com>
2024-11-01 23:58:51 +0900

    health check 경로 추가

---
+++ src/main/java/PodoeMarket/podoemarket/controller/TestController.java
@@ -0,0 +1,14 @@
+package PodoeMarket.podoemarket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Application is running";
+    }
+}
