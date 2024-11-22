20fe42fcede2cb57f2b26e50a378d09b5d9c06c9
pipi-shortstocking <jannie435@naver.com>
2024-11-23 00:58:49 +0900

    fix: post order/item orderId 추가(#185)

--- src/main/java/PodoeMarket/podoemarket/dto/response/OrderCompleteDTO.java
+++ src/main/java/PodoeMarket/podoemarket/dto/response/OrderCompleteDTO.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderCompleteDTO {
+    private Long Id;
     private LocalDateTime orderDate;
     private Long orderNum;
     private String title;
