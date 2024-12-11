fc4d61bcd81185cd76f199f17f61b87fc755b1c7
pipi-shortstocking <jannie435@naver.com>
2024-12-11 21:46:30 +0900

    fix: preview 속도 증가(#188)

--- src/main/java/PodoeMarket/podoemarket/config/WebConfig.java
+++ src/main/java/PodoeMarket/podoemarket/config/WebConfig.java
@@ -54,8 +54,8 @@ public class WebConfig implements WebMvcConfigurer {
 
         // cors 설정
         config.setAllowCredentials(true);
-//        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
-        config.setAllowedOriginPatterns(Arrays.asList("https://www.podo-store.com"));
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
+//        config.setAllowedOriginPatterns(Arrays.asList("https://www.podo-store.com"));
         config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setExposedHeaders(Arrays.asList("X-Total-Pages"));
