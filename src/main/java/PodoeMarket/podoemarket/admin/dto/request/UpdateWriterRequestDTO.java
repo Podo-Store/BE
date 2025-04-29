722e863e282fe36d998c618bbe08fd23383cb66b
pipi-shortstocking <jannie435@naver.com>
2025-04-29 16:16:33 +0900

    feat: 어드민 작가명 변경(#268)

---
+++ src/main/java/PodoeMarket/podoemarket/admin/dto/request/UpdateWriterRequestDTO.java
@@ -0,0 +1,17 @@
+package PodoeMarket.podoemarket.admin.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateWriterRequestDTO {
+    private UUID productId;
+    private String writer;
+}
