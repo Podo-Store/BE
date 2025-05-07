c953b9281bb6c18477d0b91ceff8125183eeeae7
Jihoony_.c <66457014+cjh-19@users.noreply.github.com>
2025-05-08 01:09:07 +0900

    chore: docker network 생성 로직

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,13 @@ cd /home/ubuntu/app
 # 환경변수 DOCKER_APP_NAME을 spring으로 설정
 DOCKER_APP_NAME=spring
 
+# 네트워크 존재 여부 확인 및 생성
+NETWORK_NAME="app-network"
+
+if ! sudo docker network ls | grep -q "$NETWORK_NAME"; then
+    echo "Docker 네트워크가 없습니다. $NETWORK_NAME를 생성합니다."
+    sudo docker network create $NETWORK_NAME
+fi
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
