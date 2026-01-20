d62a6ccb86453d4b1edc953c34e5bcb233e85129
pipi-shortstocking <jannie435@naver.com>
2026-01-21 02:40:48 +0900

    fix: 배포 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,8 @@ exec >>"$LOG" 2>&1
 
 echo "=== ApplicationStart $(date '+%F %T') ==="
 
-## ✅ 실제 앱 경로
-#cd /data/home/ubuntu/app
+cd /opt/codedeploy-agent/deployment-root/*/*/deployment-archive
 
-# ✅ docker 명령 고정
 DOCKER="docker"
 DC="docker-compose"
 
@@ -18,39 +16,30 @@ APP_NAME=spring
 NETWORK_NAME=app-network
 REDIS_CONTAINER_NAME=redis
 
-# .env 확인
-if [ ! -f .env ]; then
-  echo "[ERROR] .env not found"
-  exit 1
-fi
-
 # 네트워크 생성
 if ! $DOCKER network ls --format '{{.Name}}' | grep -qx "$NETWORK_NAME"; then
   echo "[INFO] create network $NETWORK_NAME"
   $DOCKER network create $NETWORK_NAME
 fi
 
-# Redis 실행
+# Redis
 if ! $DOCKER ps --filter "name=^/${REDIS_CONTAINER_NAME}$" --filter "status=running" -q | grep -q .; then
   echo "[INFO] start redis"
-  $DC --env-file .env -f docker-compose.redis.yml up -d
+  $DC -f docker-compose.redis.yml up -d
   sleep 10
 fi
 
-# BLUE / GREEN 판단
 BLUE_RUNNING=$($DOCKER ps --filter "name=${APP_NAME}-blue" --filter "status=running" -q)
 
 if [ -z "$BLUE_RUNNING" ]; then
   echo "[INFO] deploy BLUE"
-  $DC -p ${APP_NAME}-blue -f docker-compose.blue.yml --env-file .env up -d --build
+  $DC -p ${APP_NAME}-blue -f docker-compose.blue.yml up -d --build
   sleep 20
-  echo "[INFO] stop GREEN"
   $DC -p ${APP_NAME}-green -f docker-compose.green.yml down || true
 else
   echo "[INFO] deploy GREEN"
-  $DC -p ${APP_NAME}-green -f docker-compose.green.yml --env-file .env up -d --build
+  $DC -p ${APP_NAME}-green -f docker-compose.green.yml up -d --build
   sleep 20
-  echo "[INFO] stop BLUE"
   $DC -p ${APP_NAME}-blue -f docker-compose.blue.yml down || true
 fi
 
