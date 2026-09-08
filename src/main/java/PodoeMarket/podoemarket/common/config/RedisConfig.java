e37c7f9f60f2f5a6f4076c387c9db52d0085afbb
pipi-shortstocking <jannie435@naver.com>
2026-09-08 23:00:39 +0900

    refactor: redis data 지연으로 인한 화면 로딩 지연 차단

--- src/main/java/PodoeMarket/podoemarket/common/config/RedisConfig.java
+++ src/main/java/PodoeMarket/podoemarket/common/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package PodoeMarket.podoemarket.common.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -15,10 +20,26 @@ public class RedisConfig {
     private String redisHost;
     @Value("${spring.data.redis.port}")
     private int redisPort;
+    @Value("${spring.data.redis.timeout:300ms}")
+    private Duration commandTimeout;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        // 연결 단계 블로킹도 command timeout과 동일하게 제한
+        ClientOptions clientOptions = ClientOptions.builder()
+                .socketOptions(SocketOptions.builder()
+                        .connectTimeout(commandTimeout)
+                        .build())
+                .build();
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(commandTimeout) // 기본값 60s -> 명시적으로 300ms
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(serverConfig, clientConfig);
         factory.setValidateConnection(true);
         return factory;
     }
